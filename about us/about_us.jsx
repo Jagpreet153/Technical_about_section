d10802bc41fa6bf2850ac89b45f68eb57e943f61
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2023-09-02 16:08:43 +0530

    Added About Section

---
+++ about us/about_us.jsx
@@ -0,0 +1,15 @@
+import './about_us.scss';
+function About()
+{
+    return(
+        <div className="about_us">
+            <div className='content'>
+                
+                <p className='para_head'>About Us</p>
+                <p className='para'>The Technical Club of Indian Institute of Information Technology (IIIT) Bhagalpur student organizations that focus on various aspects of technology, programming, engineering, and innovation.</p>
+                <p className='para'>These clubs often organize workshops, seminars, hackathons, coding competitions, and other technical events to enhance the skills and knowledge of students in the field of technology. They can also provide a platform for students to collaborate on projects, learn from each other, and stay updated with the latest trends in the tech industry</p>
+            </div>
+        </div>
+    );
+}
+export default About;
